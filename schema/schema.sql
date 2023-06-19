199f89d8a7cb200c001a5f9647aa31562fc0ccd0
Aaron Henderson <ahenderson@Aarons-Mac-mini.hsd1.mi.comcast.net>
2023-06-18 21:11:01 -0400

    schema update

--- schema/schema.sql
+++ schema/schema.sql
@@ -16,6 +16,29 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+DROP DATABASE hendtlpg_chores_db;
+--
+-- Name: hendtlpg_chores_db; Type: DATABASE; Schema: -; Owner: ahenderson
+--
+
+CREATE DATABASE hendtlpg_chores_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = icu LOCALE = 'en_US.UTF-8' ICU_LOCALE = 'en-US';
+
+
+ALTER DATABASE hendtlpg_chores_db OWNER TO ahenderson;
+
+\connect hendtlpg_chores_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 --
 -- Name: gen_random_uuid(); Type: FUNCTION; Schema: public; Owner: ahenderson
 --
@@ -32,16 +55,26 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: adopt; Type: TABLE; Schema: public; Owner: ahenderson
+--
+
+CREATE TABLE public.adopt (
+    adoption_code uuid DEFAULT gen_random_uuid() NOT NULL,
+    family_unit uuid NOT NULL
+);
+
+
+ALTER TABLE public.adopt OWNER TO ahenderson;
+
+--
 -- Name: auth; Type: TABLE; Schema: public; Owner: ahenderson
 --
 
 CREATE TABLE public.auth (
     uuid uuid DEFAULT public.gen_random_uuid() NOT NULL,
-    username character varying NOT NULL,
     hash character varying NOT NULL,
     ip inet NOT NULL,
-    registered timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    name character varying NOT NULL
+    registered timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 
@@ -72,8 +105,7 @@ ALTER TABLE public.chores OWNER TO ahenderson;
 
 CREATE TABLE public.family (
     unit uuid NOT NULL,
-    unit_member uuid NOT NULL,
-    unit_permission character varying(1) NOT NULL
+    family_name character varying NOT NULL
 );
 
 
@@ -92,6 +124,29 @@ CREATE TABLE public.sessions (
 
 
 ALTER TABLE public.sessions OWNER TO hendtlpg_chores;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: ahenderson
+--
+
+CREATE TABLE public.users (
+    username character varying NOT NULL,
+    name character varying NOT NULL,
+    uuid uuid NOT NULL,
+    family_unit uuid,
+    family_permission character varying(1)
+);
+
+
+ALTER TABLE public.users OWNER TO ahenderson;
+
+--
+-- Name: adopt adopt_pkey; Type: CONSTRAINT; Schema: public; Owner: ahenderson
+--
+
+ALTER TABLE ONLY public.adopt
+    ADD CONSTRAINT adopt_pkey PRIMARY KEY (adoption_code);
+
 
 --
 -- Name: auth auth_pkey; Type: CONSTRAINT; Schema: public; Owner: ahenderson
@@ -114,7 +169,22 @@ ALTER TABLE ONLY public.chores
 --
 
 ALTER TABLE ONLY public.family
-    ADD CONSTRAINT family_pkey PRIMARY KEY (unit, unit_member);
+    ADD CONSTRAINT family_pkey PRIMARY KEY (unit);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: ahenderson
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (uuid);
+
+
+--
+-- Name: DATABASE hendtlpg_chores_db; Type: ACL; Schema: -; Owner: ahenderson
+--
+
+GRANT ALL ON DATABASE hendtlpg_chores_db TO hendtlpg_chores;
 
 
 --
@@ -124,6 +194,13 @@ ALTER TABLE ONLY public.family
 REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- Name: TABLE adopt; Type: ACL; Schema: public; Owner: ahenderson
+--
+
+GRANT ALL ON TABLE public.adopt TO hendtlpg_chores;
 
 
 --
@@ -145,6 +222,13 @@ GRANT ALL ON TABLE public.chores TO hendtlpg_chores;
 --
 
 GRANT ALL ON TABLE public.family TO hendtlpg_chores;
+
+
+--
+-- Name: TABLE users; Type: ACL; Schema: public; Owner: ahenderson
+--
+
+GRANT ALL ON TABLE public.users TO hendtlpg_chores;
 
 
 --
